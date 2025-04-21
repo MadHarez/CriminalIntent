ec0fc24e182de621e3fac76e46bbb8c56e51b861
MadHarez <AuraLious1019@163.com>
2025-04-21 17:36:39 +0800

    original main

---
+++ app/src/main/java/com/example/criminalintent/crimeinalntent2/Crime.kt
@@ -0,0 +1,16 @@
+package com.example.criminalintent.crimeinalntent2
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+import java.util.UUID
+
+@Entity
+data class Crime(
+    @PrimaryKey
+    val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var date: Date = Date(),
+    var isSolved: Boolean = false,
+    var suspect: String = ""
+)
---
+++ app/src/main/java/com/example/criminalintent/crimeinalntent2/CrimeListViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.criminalintent.crimeinalntent2
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
+}
